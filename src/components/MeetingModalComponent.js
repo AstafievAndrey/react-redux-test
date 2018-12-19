d8c325ae04a37cbd96c690d41a5cc0c6c4e73d92
Андрей Астафьев <carrats@yandex.ru>
2018-12-20 00:44:36 +0300

    разные проверки в модальном окне встреч

--- src/components/MeetingModalComponent.js
+++ src/components/MeetingModalComponent.js
@@ -44,11 +44,8 @@ class MeetingModalComponent extends Component {
       case 'members':
         setMember(target.getAttribute('data-key'), 'name', target.value);
         break;
-      case 'checkMember':
-        setMember(target.getAttribute('data-key'), 'active', target.checked);
-        this.forceUpdate();
-        break;
       default:
+        console.log(target.checkValidity());
         if (!target.checkValidity()) {
           target.style.borderColor = 'red';
         } else {
@@ -67,53 +64,65 @@ class MeetingModalComponent extends Component {
 
   checkActive() {
     const { date } = this.props;
-    const timeBegin = this.state.timeBegin.split(':');
-    const timeEnd = this.state.timeEnd.split(':');
+    let timeBegin = null,
+      timeEnd = null;
+    timeBegin = ((timeBegin = this.state.timeBegin.split(':')),
+    new Date(date).setHours(timeBegin[0], timeBegin[1]));
+    timeEnd = ((timeEnd = this.state.timeEnd.split(':')),
+    new Date(date).setHours(timeEnd[0], timeEnd[1]));
     let inactive = false;
     let active = false;
-    if (date < new Date().setHours(0, 0)) {
+    if (date < new Date().setHours(0, 0) || timeEnd < new Date()) {
       inactive = true;
     }
-    if (
-      new Date(date).setHours(timeBegin[0], timeBegin[1]) < new Date() &&
-      new Date(date).setHours(timeEnd[0], timeEnd[1]) > new Date()
-    ) {
+    if (timeBegin < new Date() && timeEnd > new Date()) {
       active = true;
     }
     return { inactive, active };
   }
 
   ckeckBox = key => {
-    console.log(key);
+    const checkActive = this.checkActive();
     let members = [...this.state.members];
     members[key]['active'] = !members[key]['active'];
-    this.setState({ members });
+    checkActive.active && this.setState({ members });
   };
 
   renderMeemberInput(value, key) {
-    // const { date } = this.props;
     let disabled = false;
     const checkActive = this.checkActive();
     if (checkActive.inactive === true || checkActive.active === true) {
       disabled = true;
     }
-    console.log(value, key);
     return (
       <React.Fragment key={key}>
-        <span onClick={() => this.ckeckBox(key)}>{String(value.active)}</span>
-        <input
-          className="input membersInput"
-          onChange={this.handleChange}
-          data-key={key}
-          value={value.name}
-          disabled={disabled}
-          name="members"
-        />
+        <span onClick={() => this.ckeckBox(key)}>
+          <input
+            className={`${
+              (checkActive.inactive || checkActive.active) && value.name !== ''
+                ? ''
+                : 'none'
+            }`}
+            type={'checkbox'}
+            checked={value.active}
+            readOnly
+          />
+          <input
+            className="input membersInput"
+            onChange={this.handleChange}
+            onClick={() => this.ckeckBox(key)}
+            data-key={key}
+            value={value.name}
+            disabled={disabled}
+            name="members"
+          />
+        </span>
       </React.Fragment>
     );
   }
 
   renderMembers() {
+    const checkActive = this.checkActive();
     let result = '';
     if (this.state.members) {
       result = this.state.members.map((item, index) => {
@@ -122,12 +131,19 @@ class MeetingModalComponent extends Component {
     } else {
       result = this.renderMeemberInput(undefined, 0);
     }
-    return (
-      <React.Fragment>
-        {result}
+    const add = () => {
+      return (
         <span className="addMember" onClick={this.addMember}>
           Добавить участника
         </span>
+      );
+    };
+    return (
+      <React.Fragment>
+        {result}
+        {checkActive.inactive === false &&
+          checkActive.active === false &&
+          add()}
       </React.Fragment>
     );
   }
@@ -138,6 +154,19 @@ class MeetingModalComponent extends Component {
   };
 
   renderInputTime(name) {
+    const { date } = this.props;
+    const timeBegin = this.state.timeBegin.split(':');
+    let min = `${
+      new Date(date) > new Date() ? '00:00' : new Date().getHours()
+    }:00`;
+    if (name === 'timeEnd') {
+      let dt = new Date(
+        new Date(date).setHours(timeBegin[0], parseInt(timeBegin[1]) + 30)
+      );
+      min = `${
+        dt.getHours() < 10 ? '0' : ''
+      }${dt.getHours()}:${dt.getMinutes()}`;
+    }
     return (
       <input
         className="input"
@@ -146,7 +175,7 @@ class MeetingModalComponent extends Component {
         type="time"
         step="1800"
         pattern="[0-24]{2}:(30|00)"
-        min={`${new Date().getHours()}:00`}
+        min={min}
         name={name}
       />
     );
@@ -174,14 +203,21 @@ class MeetingModalComponent extends Component {
   }
 
   renderAction() {
+    const checkActive = this.checkActive();
+    const save = () => {
+      return (
+        <div className="btn btn-primary" onClick={this.handleSubmit}>
+          сохранить
+        </div>
+      );
+    };
     return (
       <div className="meetingModalAction">
         <div className="btn" onClick={this.hideModal}>
           отмена
         </div>
-        <div className="btn btn-primary" onClick={this.handleSubmit}>
-          сохранить
-        </div>
+        {(checkActive.inactive === false || checkActive.active === true) &&
+          save()}
       </div>
     );
   }
